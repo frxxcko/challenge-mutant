88a09b189af13ab47bf252be3c27f5dc1838ddac
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-15 16:18:18 -0300

    Create a Theme obj to be consistent with color branding

---
+++ src/components/UI/Theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    primary: '#8758FF',
+    secondary: '#0073A3',
+    background: '#fff',
+    text_primary: '#191B1D',
+    text_secondary: '#1DBEB4',
+    content_background: '#FDF6EC'
+}
