9141fa69884427cc86fd0dad72fc177943aa621f
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-15 19:12:27 -0300

    Change secondary value in Theme component

--- src/components/UI/Theme.js
+++ src/components/UI/Theme.js
@@ -1,6 +1,6 @@
 export const theme = {
     primary: '#8758FF',
-    secondary: '#0073A3',
+    secondary: 'rgb(141, 141, 141)',
     background: '#fff',
     text_primary: '#191B1D',
     text_secondary: '#1DBEB4',
