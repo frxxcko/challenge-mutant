2b8dbd4f635e0e13afe1ae97ba435c28aa72f69d
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-15 16:17:11 -0300

    Create StyledCard styled component in UI > card folder

---
+++ src/components/UI/card/StyledCard.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledCard = styled.div`
+border-radius: 5px;
+box-shadow: 0px 2px 5px 0px rgba( 0, 0, 0, 0.2);
+overflow: hidden;
+margin: 10px 20px;
+width: 40rem;
+a{
+    text-decoration: none;
+}
+
+`
