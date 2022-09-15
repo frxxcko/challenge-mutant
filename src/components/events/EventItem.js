a2eb55d06cea94290a6ac9d110e5f4e346b7e339
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-15 20:29:07 -0300

    Estructure for better implement of styling

--- src/components/events/EventItem.js
+++ src/components/events/EventItem.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Card from '../UI/card/Card'
 import { EventContainer } from './StyledEvent'
 
-
-const EventItem = ({ type, title, img, url, address, place, location, }) => {
+const EventItem = ({ type, title, img, lowestPrice, location, datetime }) => {
     const typeParsed = type.replace(/_/g, ' ').split(' ').map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(' ');
 
     const handleClick = () => {
@@ -13,11 +12,16 @@ const EventItem = ({ type, title, img, url, address, place, location, }) => {
     return (
         <Card>
             <EventContainer onClick={handleClick}>
-                <img src={img} alt="" />
-                <div>
-                    <h1>{title}</h1>
-                    <h5>{typeParsed}</h5>
-
+                <img src={img} alt={`${title} ${type}`} aria-hidden='true' />
+                <div className='eventInfo'>
+                    <div>
+                        <h1>{title}</h1>
+                        <h2>{new Date(datetime).toLocaleString('en-US', { day: '2-digit', month: 'short' })} · {location}</h2>
+                    </div>
+                    <div>
+                        <span>{lowestPrice ? `From $${lowestPrice}` : 'Find tickets'}</span>
+                        <p>{typeParsed}</p>
+                    </div>
                 </div>
             </EventContainer>
         </Card>
