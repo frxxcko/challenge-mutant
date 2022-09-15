7d5d49aee7938c36775fc5db9ea61f0e8df34895
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-15 16:14:20 -0300

    App.js implementing useEffect to retrieve events data from API Endpoint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from 'react-router-dom'
+import Home from "./components/home/Home";
+import { AxiosInstance } from "./helpers/AxiosHelper";
 
 function App() {
+  const [events, setEvents] = useState(null)
+
+  useEffect(() => {
+    try {
+      AxiosInstance.get(`/events?client_id=${process.env.REACT_APP_API_CLIENT_ID}`).then(({ data }) => setEvents(data.events))
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<Home events={events} />} />
+        <Route path="/challenge-mutant" element={<Home events={events} />} />
+      </Routes>
     </>
   );
 }
