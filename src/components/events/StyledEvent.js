566b465b0d4835785fb0fbb43749d35ae4b183fd
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-15 20:28:12 -0300

    General styling in StyledEvents component

--- src/components/events/StyledEvent.js
+++ src/components/events/StyledEvent.js
@@ -1,20 +1,41 @@
 import styled from "styled-components";
 import { theme } from '../UI/Theme'
 
+export const StyledTitle = styled.h1`
+    color: ${theme.primary};
+    margin: 20px;
+    cursor: default;
+    width: 69%;
+`
+
 export const EventListContainer = styled.div`
     color: ${theme.primary};
-    h1{
-        margin: 20px;
-    }
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 `
 export const EventContainer = styled.div`
     display: flex;
     h1{
         margin: 10px;
-        font-size: 1.3rem;
+        font-size: 1.4rem;
     }
-    h5{
-        margin: 0px 10px;
+    h2{
+        font-size: 1rem;
+        margin: 10px;
+        color: ${theme.secondary}
+    }
+    p{
+        margin: 10px;
         color: ${theme.text_secondary};
     }
-    `
+    span{
+        margin: 10px;
+        color: ${theme.primary}
+    }
+    .eventInfo{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+`
