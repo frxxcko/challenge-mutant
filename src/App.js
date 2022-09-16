c656d0ebf31238dcb1129746f1cf02465ea31d10
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-16 01:59:14 -0300

    Create Route for 404 errors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from "./components/home/Home";
 import { StyledTemplateResponsive } from "./components/template/StyledTemplateResponsive";
 import { AxiosInstance } from "./helpers/AxiosHelper";
+import ErrorPage from './components/error/ErrorPage'
 
 function App() {
   const [events, setEvents] = useState(null);
@@ -13,19 +14,19 @@ function App() {
       setLoaded(false)
       AxiosInstance.get(`/events?client_id=${process.env.REACT_APP_API_CLIENT_ID}`)
         .then(({ data }) => setEvents(data.events))
-        .then(() => setLoaded(true))
         .catch(err => console.log(err))
+        .finally(() => { setLoaded(true) })
     } catch (error) {
       console.log(error)
     }
   }, [])
-
 
   return (
     <StyledTemplateResponsive>
       <Routes>
         <Route path="/" element={<Home loaded={loaded} events={events} />} />
         <Route path="/challenge-mutant" element={<Home loaded={loaded} events={events} />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </StyledTemplateResponsive>
   );
